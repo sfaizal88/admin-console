38d7826bb8117b568aa78832abef257a36f3e761
Iwaymen <iwaymen@schrodingernft.ai>
2024-03-25 10:28:18 +0800

    first commit

--- src/api/constants.js
+++ src/api/constants.js
@@ -2,10 +2,15 @@ export const EXPERIMENT_SUBMIT_API = "/experiments/start-task";
 export const EXPERIMENT_LIST_API = "/experiments/get-list";
 export const UPDATE_EXPERIMENT_API = "/image/generate";
 export const GET_EXPERIMENT_BY_ID_API = "/experiments/result/";
-export const LOGIN_API = "/experiments/auth";
+export const LOGIN_API = "https://accounts.zoho.com/oauth/v2/token";
 
 export const JSONHeader = {
     headers: {
         'Content-Type': 'application/json',
+    },
+}
+export const JSONFormHeader = {
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
     },
 }
--- src/view/pages/credential/login/index.jsx
+++ src/view/pages/credential/login/index.jsx
@@ -8,7 +8,7 @@
 // GENERIC IMPORT
 import React, {useState, useEffect} from 'react';
 import {Box, TextField, Button} from '@mui/material';
-import {useNavigate, useLocation} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
 // COMPONENT IMPORT
@@ -23,12 +23,10 @@ import Auth from '../auth';
 import LogoIcon from '../../../../assets/img/aelf-logo.png';
 
 // UTILS
-// import {LOGIN_TOKEN} from '../../../../utils/constants';
 import useNotification from '../../../../utils/notification';
-// import {FEATURE} from '../../../../utils/feature';
 
 // API
-import {LOGIN_API, JSONHeader} from '../../../../api/constants';
+import {LOGIN_API, JSONFormHeader} from '../../../../api/constants';
 
 // STYLE IMPORT
 import useStyles from './styles';
@@ -37,51 +35,13 @@ const LoginPage = () => {
   // DECLARE STYLE
   const classes = useStyles();
 
-  // PARAMS
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const password = queryParams.get('password');
-  
-  // const { password } = useParams();
-
   // DECLARE NOTIFICATION AND NAVIDATE
   const setNotification = useNotification();
   const navigate = useNavigate();
 
   // STATE VARIABLE
   const [isLoading, setLoading] = useState(false);
-  const [token, setToken] = useState(password || '');
-
-  const onLogin = async () => {
-    if (!isFormValid()) {
-      return;
-    }
-    setLoading(true);
-    try {
-      const response = await axios.post(LOGIN_API,{token}, JSONHeader);
-      // RESULT 0 -> FAILURE
-      // RESULT 1 -> SUCCESS
-      if (response.data.result) {
-        const responseToken = response.data;
-        navigate(PATH.HOME_PATH);
-        localStorage.setItem('token', responseToken);
-      } else {
-        setNotification.error("Invalid login credentials");
-      }
-      // STATIC CODE WHEN API NOT READY
-      /** if (FEATURE.DISABLE_LOGIN_CHECK || token === LOGIN_TOKEN) {
-        navigate(PATH.HOME_PATH);
-        localStorage.setItem('token', token);
-      } else {
-        setNotification.error("Invalid login credentials");
-      } */
-    } catch(error) {
-      console.log("Error: ", error);
-      setNotification.error("Something went wrong. Please try again later.");
-    } finally {
-      setLoading(false);
-    }
-  };
+  const [email, setEmail] = useState();
 
   const isFormValid = () => {
     if (!token.trim().length) {
@@ -109,7 +69,7 @@ const LoginPage = () => {
 
   // After successful authentication, Zoho will redirect back to your website with an authorization code
   // You need to handle this code and exchange it for an access token
-  const handleAuthorizationCode = () => {
+  const handleAuthorizationCode = async () => {
     const url = window.location.href;
     const queryString = url.substring(url.indexOf('?') + 1);
     const params = new URLSearchParams(queryString);
@@ -118,36 +78,24 @@ const LoginPage = () => {
     console.log("url: ", url);
     console.log("params: ", params);
     if (code) {
-    // Make a POST request to exchange the code for an access token
-    fetch('https://accounts.zoho.com/oauth/v2/token', {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({
+      // Make a POST request to exchange the code for an access token
+      const param = {
         code: code,
         client_id: '1000.EL550QWO3I79EANZ409YGYIOVT838M',
         client_secret: 'e9f694011af6e2684ceb25526a9cd82c6adb08bdf8',
         redirect_uri: 'https://sfaizal88.github.io/admin-console/',
         grant_type: 'authorization_code'
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Handle the response, save the access token, etc.
-        console.log('Access Token:', data.access_token);
-        // setIsAuthenticated(true);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+      };
+      const response = await axios.post(LOGIN_API, {...param}, JSONFormHeader);
+      console.log('response: ', response.data);
+      if (response.data) {
+        console.log('response: ', response.data);
+      }
     }
   };
 
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
-      console.log("entered")
-      // onLogin();
       // mockLogin();
       zohoLogin();
     }
@@ -180,8 +128,8 @@ const LoginPage = () => {
               <Box className={classes.loginTitle}>Login</Box>
               <Box className={classes.loginSubTitle}>Don&apos;t have an account? Please contact our <Box className={classes.link} onClick={contactAdmin}>admin team</Box></Box>
             </Box>
-            <TextField type='password' label="Password" variant="outlined" 
-            fullWidth placeholder='Please enter the token' value={token} size='small'
+            <TextField label="Email" variant="outlined" 
+            fullWidth placeholder='Please enter the email' value={token} size='small'
             onChange={(event) => setToken(event.target.value)} onKeyDown={handleKeyDown}/>
             <Button variant="contained" fullWidth onClick={onLogin}>Login</Button>
           </Box>
