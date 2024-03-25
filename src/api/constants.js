416bd1f96e897d0b729504c1aa674f1a87920558
Iwaymen <iwaymen@schrodingernft.ai>
2024-03-25 11:14:58 +0800

    first commit

--- src/api/constants.js
+++ src/api/constants.js
@@ -2,7 +2,7 @@ export const EXPERIMENT_SUBMIT_API = "/experiments/start-task";
 export const EXPERIMENT_LIST_API = "/experiments/get-list";
 export const UPDATE_EXPERIMENT_API = "/image/generate";
 export const GET_EXPERIMENT_BY_ID_API = "/experiments/result/";
-export const LOGIN_API = "https://accounts.zoho.com/oauth/v1/token";
+export const LOGIN_API = "https://accounts.zoho.com/oauth/v2/token";
 
 export const JSONHeader = {
     headers: {
@@ -17,6 +17,11 @@ export const JSONFormHeader = {
 // Server based
 // 1000.EL550QWO3I79EANZ409YGYIOVT838M
 // e9f694011af6e2684ceb25526a9cd82c6adb08bdf8
-export const CLIENT_ID = '1000.OGM448XR5YVKYBUQYC4OVBH4781JLE';
-export const CLIENT_SECRET = '554d05d27738a6b3cf178224dca9c26cd2dc61e2b4';
+
+
+// Client based
+// 1000.OGM448XR5YVKYBUQYC4OVBH4781JLE
+// 554d05d27738a6b3cf178224dca9c26cd2dc61e2b4
+export const CLIENT_ID = '1000.EL550QWO3I79EANZ409YGYIOVT838M';
+export const CLIENT_SECRET = 'e9f694011af6e2684ceb25526a9cd82c6adb08bdf8';
 export const REDIRECT_URL = 'https://sfaizal88.github.io/admin-console/';
