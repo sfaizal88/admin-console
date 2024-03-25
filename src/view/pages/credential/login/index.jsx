01f132c6a0fdfe45288673ce5ebecdcb6f7260e4
Iwaymen <iwaymen@schrodingernft.ai>
2024-03-25 10:01:37 +0800

    first commit

--- src/view/pages/credential/login/index.jsx
+++ src/view/pages/credential/login/index.jsx
@@ -101,11 +101,48 @@ const LoginPage = () => {
   const zohoLogin = () => {
     const responseType = 'response_type=code';
     const clientId = 'client_id=1000.EL550QWO3I79EANZ409YGYIOVT838M';
-    const redirectUri = 'redirect_uri=https://sfaizal88.github.io/admin-console/zohoAuth';
+    const redirectUri = 'redirect_uri=https://sfaizal88.github.io/admin-console/';
     const scope = 'scope=ZohoAssist.userapi.READ';
     // Redirect the user to Zoho's authorization URL
     window.location.href = `https://accounts.zoho.com/oauth/v2/auth?${responseType}&${clientId}&${redirectUri}&${scope}`;
   }
+
+  // After successful authentication, Zoho will redirect back to your website with an authorization code
+  // You need to handle this code and exchange it for an access token
+  const handleAuthorizationCode = () => {
+    const url = window.location.href;
+    const queryString = url.substring(url.indexOf('?') + 1);
+    const params = new URLSearchParams(queryString);
+    const code = params.get('code');
+    console.log("Code: ",code);
+    console.log("url: ", url);
+    console.log("params: ", params);
+    if (code) {
+    // Make a POST request to exchange the code for an access token
+    fetch('https://accounts.zoho.com/oauth/v2/token', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+        code: code,
+        client_id: '1000.EL550QWO3I79EANZ409YGYIOVT838M',
+        client_secret: 'e9f694011af6e2684ceb25526a9cd82c6adb08bdf8',
+        redirect_uri: 'https://sfaizal88.github.io/admin-console/',
+        grant_type: 'authorization_code'
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Handle the response, save the access token, etc.
+        console.log('Access Token:', data.access_token);
+        setIsAuthenticated(true);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+    }
+  };
 
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
@@ -119,6 +156,12 @@ const LoginPage = () => {
   const contactAdmin = () => {
     setNotification.success("Please contact Steve.");
   }
+
+  // Call the function to exchange authorization code for access token
+  // This should be called when the component mounts
+  useEffect(() => {
+      handleAuthorizationCode();
+  }, []);
 
   return (
     <>
