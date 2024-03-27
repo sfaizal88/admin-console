ecdc4a7b806c7b345feb2ef66952549252c1909a
Iwaymen <iwaymen@schrodingernft.ai>
2024-03-27 11:55:49 +0800

    feature: API update

--- src/api/constants.js
+++ src/api/constants.js
@@ -2,8 +2,8 @@ export const EXPERIMENT_SUBMIT_API = "/experiments/start-task";
 export const EXPERIMENT_LIST_API = "/experiments/get-list";
 export const UPDATE_EXPERIMENT_API = "/image/generate";
 export const GET_EXPERIMENT_BY_ID_API = "/experiments/result/";
-export const LOGIN_API = "https://0d29-154-91-47-139.ngrok-free.app/zoho/auth";
-export const ALL_ACCOUNTS_API = "https://0d29-154-91-47-139.ngrok-free.app/zoho/account";
+export const LOGIN_API = "https://29eb-202-156-61-238.ngrok-free.app/zoho/auth";
+export const ALL_ACCOUNTS_API = "https://29eb-202-156-61-238.ngrok-free.app/zoho/account";
 export const JSONHeader = {
     headers: {
         'Content-Type': 'application/json',
